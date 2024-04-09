e9ec3abfa193dc49c52cb8d35cf1a22b8fc355f7
sean <mslee@stcomm.kr>
2024-04-09 11:11:45 +0900

    Todo list 추가 수정 삭제 To do list 추가 수정 삭제 2

--- JS/todolist.js
+++ JS/todolist.js
@@ -12,7 +12,11 @@ $("#addTextBtn").on('click', function(){
     } else {
         let addHtml = '<li>';
         addHtml += '<input type="checkbox" name="" id="">';
+        addHtml += '<span class="todoText">';
         addHtml += addText
+        addHtml += '</span>';
+        addHtml += '<button class="completBtn">완료</button>';
+        addHtml += '<button class="modifyBtn">수정</button>';
         addHtml += '<button class="removeBtn">삭제</button>';
         addHtml += '</li>';
 
@@ -25,8 +29,31 @@ $("#cancelBtn").on('click', function(){
 });
 
 $(document).on('click', '.removeBtn', function(index){
-    let num = $(".removeBtn").index(this);
-    console.log(num);
-    let todoList = $(".list li").get(num);
-    todoList.remove();
+    // let num = $(".removeBtn").index(this);
+    // console.log(num);
+    // let todoList = $(".list li").get(num);
+    // todoList.remove();
+    let parentsLi = $(this).parents('li');
+    parentsLi.remove();
+})
+
+$(document).on('click', '.modifyBtn', function(index){
+    // console.log('수정버튼');
+    $(this).parents('li').addClass('modify');
+    let spanTag = $(this).parents('li').find('.todoText');
+    let orgText = spanTag.text();
+    console.log(orgText);
+    let inputHtml = '<input type="text" class="" value="'+orgText+'" />';
+    spanTag.html(inputHtml);
+
+
+})
+
+$(document).on('click', '.completBtn', function(index){
+    //console.log('완료버튼');
+    $(this).parents('li').removeClass('modify');
+    let spanTag = $(this).parents('li').find('.todoText');
+    let afterText = spanTag.find('input').val();
+    spanTag.html(afterText);
+
 })
